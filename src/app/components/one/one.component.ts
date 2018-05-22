2b882e2aed7ed2e477d7a5357fae88f5e55bc08e
NaliniRanawat <naliniranawat@yahoo.com>
2018-05-22 04:39:32 -0700

    first commit

---
+++ src/app/components/one/one.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../service/user.service';
+
+@Component({
+  selector: 'app-one',
+  templateUrl: './one.component.html',
+  styleUrls: ['./one.component.css']
+})
+export class OneComponent implements OnInit {
+user:string;
+
+  constructor(private userService:UserService) { }
+
+  ngOnInit() {
+    this.user = this.userService.getFirstUser();
+  }
+
+}
